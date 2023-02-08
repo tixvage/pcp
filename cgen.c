c0903aae7df71e0401f03bd24895fffed231c6d0
tixvage <mel10h3421@gmail.com>
2023-02-08 14:53:55 +0300

    move function decls to .h

--- cgen.c
+++ cgen.c
@@ -9,20 +9,6 @@
 
 FILE *f;
 
-void cgen_prepare(void);
-void cgen_structs(Checked_File *decls);
-void cgen_struct(Checked_Struct_Decl *sc);
-void cgen_functions(Checked_File *decls);
-void cgen_function(Checked_Fn_Decl *fn);
-void cgen_top_assignments(Checked_File *decls);
-void cgen_statement(Checked_Stmt stmt);
-void cgen_if_statment(Checked_If_Stmt *if_stmt);
-void cgen_for_statment(Checked_For_Stmt *for_stmt);
-void cgen_return_statment(Checked_Return_Stmt *return_stmt);
-void cgen_var_declaration(Checked_Var_Decl *var_decl);
-void cgen_var_assignment(Checked_Var_Assign *var_assign);
-void cgen_expr(Checked_Expr *expr);
-
 void cgen_prepare(void) {
     fprintf(f, "#include <stdbool.h>\n");
     fprintf(f, "#include <stdint.h>\n");
--- parser.h
+++ parser.h
@@ -28,4 +28,26 @@ typedef struct Parser {
 void init_parser(Parser *parser, Lexer lexer);
 void parser_parse(Parser *parser);
 
+Stmt parse_top_stmt(Parser *parser);
+Stmt parse_child_stmt(Parser *parser);
+Expr *parse_expr(Parser *parser);
+Expr *parse_primary_expr(Parser *parser);
+Expr *parse_comparative_expr(Parser *parser);
+Expr *parse_additive_expr(Parser *parser);
+Expr *parse_multiplicitave_expr(Parser *parser);
+Struct_Construct *parse_struct_construct_expr(Parser *parser);
+Expr *parse_cast_expr(Parser *parser);
+Var_Decl *parse_var_decl(Parser *parser);
+Fn_Decl *parse_fn_decl(Parser *parser);
+Fn_Decl *parse_extern_fn_decl(Parser *parser);
+void parse_fn_decl_args(Parser *parser, Fn_Decl *fn_decl);
+Struct_Decl *parse_struct_decl(Parser *parser);
+Return_Stmt *parse_return_stmt(Parser *parser);
+If_Stmt *parse_if_stmt(Parser *parser);
+For_Stmt *parse_for_stmt(Parser *parser);
+Var_Assign *parse_var_assign(Parser *parser);
+Func_Call *parse_func_call(Parser *parser);
+void parse_func_call_args(Parser *parser, Func_Call *func_call);
+void parse_file(Parser *parser);
+
 #endif
